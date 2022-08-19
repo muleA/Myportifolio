c036528f807372621352689793a3c00db9302b04
Mulugeta Adamu <56386485+muleA@users.noreply.github.com>
2022-08-19 16:14:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { themes } from "./theme";
 import { GlobalStyles } from "./global";
-import { CursorProvider } from "react-cursor-custom";
 import { settings } from "./portfolio";
 import ReactGA from "react-ga";
 
@@ -26,17 +25,9 @@ function App() {
       <>
         <GlobalStyles />
         <div>
-          {useCursor ? (
-            <CursorProvider
-              color={themes[theme].secondaryText}
-              ringSize={10}
-              transitionTime={75}
-            >
+         
               <Main theme={themes[theme]} setTheme={setTheme} />
-            </CursorProvider>
-          ) : (
-            <Main theme={themes[theme]} setTheme={setTheme} />
-          )}
+            
         </div>
       </>
     </ThemeProvider>
