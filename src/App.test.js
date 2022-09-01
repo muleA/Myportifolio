0b1a23fa18d4d893007c42c395b5d1ce5a9b2a26
mulugeta <mulugeta.adamu97@gmail.com>
2022-09-01 14:42:23 +0300

    Refactor usability tests. > > Co-authored-by: assefa <asefa12a19@gmail.com>

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import App from "./App";
 import { render } from "@testing-library/react";
 describe("App Component", function () {
-  it("should have hello world message", function () {
+  it("adding Test library passed successfully!", function () {
     let { getByText } = render(<App />);
     expect(getByText("Hello world React!")).toMatchInlineSnapshot(
       <h1>adding Test library passed successfully!</h1>
