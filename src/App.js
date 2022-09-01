e9fb3f2f58cb3ee25fb9ebc56bc5d2a29521ed9e
mulugeta <mulugeta.adamu97@gmail.com>
2022-09-01 10:23:03 +0300

    updated protifolio

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Main from "./containers/Main";
+import { ThemeProvider } from "styled-components";
+import { themes } from "./theme";
+import { GlobalStyles } from "./global";
+import { settings } from "./portfolio";
+import ReactGA from "react-ga";
+
+function App() {
+  useEffect(() => {
+    if (settings.googleTrackingID) {
+      ReactGA.initialize(settings.googleTrackingID, {
+        testMode: process.env.NODE_ENV === "test",
+      });
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  }, []);
+
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+
+  return (
+    <ThemeProvider theme={themes[theme]}>
+      <>
+        <GlobalStyles />
+        <div>
+          <Main theme={themes[theme]} setTheme={setTheme} />
+        </div>
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,147 @@
+import React, { useState } from "react";
+import "./Header.css";
+import { Fade } from "react-reveal";
+import { NavLink, Link } from "react-router-dom";
+import { settings } from "../../portfolio.js";
+import { CgSun } from "react-icons/cg/";
+import { HiMoon } from "react-icons/hi";
+import { style } from "glamor";
+
+function Header(props) {
+  const theme = props.theme;
+
+  const styles = style({
+    cursor: "pointer",
+    height: "40px",
+    width: "40px",
+    marginRight: "2px",
+    marginLeft: "15px",
+    paddingTop: "5px",
+    borderRadius: "50%",
+    border: "none",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: props.theme.name === "light" ? "#7CD1F7" : "#292C3F",
+    outline: "none",
+    transition: "all 0.2s ease-in-out",
+    ":hover": {
+      boxShadow: `0 3px 8px ${
+        props.theme.name === "light" ? "#F7D774" : "#646464"
+      }`,
+    },
+  });
+
+  const link = settings.isSplash ? "/splash" : "home";
+
+  const [currTheme, setCurrTheme] = useState(props.theme);
+
+  function changeTheme() {
+    if (currTheme === "light") {
+      props.setTheme("dark");
+      localStorage.setItem("theme", "dark");
+      setCurrTheme("dark");
+    } else {
+      props.setTheme("light");
+      localStorage.setItem("theme", "light");
+      setCurrTheme("light");
+    }
+  }
+
+  const icon =
+    props.theme.name === "dark" ? (
+      <HiMoon
+        strokeWidth={1}
+        size={20}
+        color={props.theme.name === "light" ? "#F9D784" : "#A7A7A7"}
+      />
+    ) : (
+      <CgSun
+        strokeWidth={1}
+        size={20}
+        color={props.theme.name === "light" ? "#F9D784" : "#A7A7A7"}
+      />
+    );
+
+  return (
+    <Fade top duration={1000} distance="20px">
+      <div>
+        <header className="header">
+          <NavLink to={link} tag={Link} className="logo">
+            <span style={{ color: theme.text }}></span>
+            <span className="logo-name" style={{ color: theme.text }}>
+              &lt;Mulugeta/&gt;
+            </span>
+            <span style={{ color: theme.text }}></span>
+          </NavLink>
+          <input className="menu-btn" type="checkbox" id="menu-btn" />
+          <label className="menu-icon" htmlFor="menu-btn">
+            <span className="navicon"></span>
+          </label>
+          <ul className="menu">
+            <li>
+              <NavLink
+                className="homei"
+                to="/home"
+                tag={Link}
+                activeStyle={{ fontWeight: "bold" }}
+                style={{ borderRadius: 5, color: theme.text }}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="ec"
+                to="/education"
+                tag={Link}
+                activeStyle={{ fontWeight: "bold" }}
+                style={{ borderRadius: 5, color: theme.text }}
+              >
+                Education and Certifications
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="xp"
+                to="/experience"
+                tag={Link}
+                activeStyle={{ fontWeight: "bold" }}
+                style={{ borderRadius: 5, color: theme.text }}
+              >
+                Skills and Experience
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="projects"
+                to="/projects"
+                tag={Link}
+                activeStyle={{ fontWeight: "bold" }}
+                style={{ borderRadius: 5, color: theme.text }}
+              >
+                My Personal Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="cr"
+                to="/contact"
+                tag={Link}
+                activeStyle={{ fontWeight: "bold" }}
+                style={{ borderRadius: 5, color: theme.text }}
+              >
+                Contacts and Resume
+              </NavLink>
+            </li>
+
+            <button {...styles} onClick={changeTheme}>
+              {icon}
+            </button>
+          </ul>
+        </header>
+      </div>
+    </Fade>
+  );
+}
+
+export default Header;
